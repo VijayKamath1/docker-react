861b5f313659f30724183ab51498ba22c47ed754
Vijay Kamath <kamatvijay@gmail.com>
2019-04-27 23:02:56 +0100

    ,initial commit

---
+++ build/precache-manifest.9e14a44f7e1edbdcb7508f2d062c0ee6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2aeeba9ec1cd15fe2e5f",
+    "url": "/static/js/main.8dbd7b77.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "2aeeba9ec1cd15fe2e5f",
+    "url": "/static/css/main.8c3ee4d6.chunk.css"
+  },
+  {
+    "revision": "b0cfc742929c82d791d78a2d0d497dd1",
+    "url": "/index.html"
+  }
+];
